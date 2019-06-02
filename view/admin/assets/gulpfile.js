fb70420d8dd507805c51d5b597fcb7fc17dacc5c
xqLi <dyermaker@163.com>
2019-06-02 18:02:43 +0800

    admin view gulp

--- view/admin/assets/gulpfile.js
+++ view/admin/assets/gulpfile.js
@@ -1,7 +1,8 @@
 "use strict";
 
+//gulp production is ran with 'type=production gulp'
+
 var gulp = require('gulp'),
-  gutil = require('gulp-util'),
   jshint = require('gulp-jshint'),
   uglify = require('gulp-uglify'),
   sass = require('gulp-sass'),
@@ -10,65 +11,75 @@ var gulp = require('gulp'),
   sourcemaps = require('gulp-sourcemaps'),
   livereload = require('gulp-livereload');
 
-gulp.task('html', function() {
+const through = require("through2");
+
+const noop = function() {
+  // just pass-through anything
+  return through.obj();
+};
+
+gulp.task('html', function () {
   gulp.src('../*.html')
-  .pipe(livereload());
+    .pipe(livereload());
 });
 
-gulp.task('css', function() {
+gulp.task('css', function () {
   gulp.src('src/scss/**/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass())
-  .pipe(sourcemaps.write())
-  .pipe(gutil.env.type === 'production' ? cssmin() : gutil.noop())
-  .pipe(gulp.dest('dist/css/'))
-  .pipe(livereload());
+    .pipe(process.env.type === 'production' ? noop() : sourcemaps.init())
+    .pipe(sass())
+    .pipe(process.env.type === 'production' ? noop() : sourcemaps.write())
+    .pipe(process.env.type === 'production' ? cssmin() : noop())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(livereload());
 });
 
-gulp.task('js', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(sourcemaps.init())
+
+gulp.task('js', function () {
+  return gulp.src('./src/js/*.js')
+    .pipe(process.env.type === 'production' ? noop() : sourcemaps.init())
     .pipe(concat('app.js'))
-    //only uglify if gulp is ran with '--type production'
-    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
-    .pipe(sourcemaps.write())
+    .pipe(process.env.type === 'production' ? noop() : sourcemaps.write())
     .pipe(gulp.dest('dist/js'))
     .pipe(livereload());
 });
 
-gulp.task('vendor-js', function() {
+gulp.task('vendor-js', function () {
   return gulp.src([
     'src/vendor/js/codemirror.js',
     'src/vendor/js/*.js'
   ])
-  .pipe(concat('vendor.js'))
-  .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
-  .pipe(gulp.dest('dist/js/'))
+    .pipe(concat('vendor.js'))
+    .pipe(process.env.type === 'production' ? uglify() : noop())
+    .pipe(gulp.dest('dist/js/'))
 });
 
-gulp.task('vendor-css', function() {
+gulp.task('vendor-css', function () {
   return gulp.src('src/vendor/css/*.css')
-  .pipe(concat('vendor.css'))
-  .pipe(gutil.env.type === 'production' ? cssmin() : gutil.noop())
-  .pipe(gulp.dest('dist/css/'))
+    .pipe(concat('vendor.css'))
+    .pipe(process.env.type === 'production' ? cssmin() : noop())
+    .pipe(gulp.dest('dist/css/'))
 });
 
-gulp.task('copy-fonts', function() {
+gulp.task('copy-fonts', function () {
   return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts/'));
 });
 
-gulp.task('copy-img', function() {
+gulp.task('copy-img', function () {
   return gulp.src('src/img/**/*.png').pipe(gulp.dest('dist/img/'));
 });
 
 //Watch task
-gulp.task('default',function() {
+gulp.task('watch', function () {
   livereload.listen();
-  gulp.watch('src/scss/**/*.scss',['css']);
-  gulp.watch('src/js/**/*.js',['js']);
-  gulp.watch('src/vendor/css/*.css',['vendor-css']);
-  gulp.watch('src/vendor/js/*.js',['vendor-js']);
-  gulp.watch('src/fonts/**/*',['copy-fonts']);
-  gulp.watch('src/img/**/*.png',['copy-img']);
-  gulp.watch('../*.html',['html']);
+  gulp.watch('./src/scss/**/*.scss', ['css']);
+  gulp.watch('./src/js/*.js', ['js']);
+  gulp.watch('./src/vendor/css/*.css', ['vendor-css']);
+  gulp.watch('./src/vendor/js/*.js', ['vendor-js']);
+  gulp.watch('./src/fonts/**/*', ['copy-fonts']);
+  gulp.watch('./src/img/**/*.png', ['copy-img']);
+  gulp.watch('../*.html', ['html']);
+});
+
+gulp.task('default', function () {
+  gulp.start('html', 'copy-img', 'copy-fonts', 'vendor-js', 'vendor-css', 'js', 'css', 'watch')
 });
